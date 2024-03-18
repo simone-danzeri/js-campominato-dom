0b9af2f2f17873a28a6b7c752c82528d7b224a4e
simone-danzeri <s.danzeri@icloud.com>
2024-03-18 12:30:42 +0100

    feat: human code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
-// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-// Ogni cella ha un numero progressivo, da 1 a 100.
-// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+
+// Creo variabile punteggio con valore 0
+// Mi creo array vuoto dove genero 16 num casuali che saranno le bombe
+    // Dove clicco ogni singola cella controllo se il numero della cella è uguale ad un elemento dell'array bombe
+    // Se clicco una cella il cui numero è presente nell'array bombe
+        // Allora ho perso
+        // Altrimenti vado avanti ed incremento la variabile punteggio di 1
+
+
+
+
 
 // Mi seleziono i due elementi del DOM che mi servono e li metto in una variabile
 const playBtn = document.querySelector('#play-btn');
@@ -12,6 +24,8 @@ console.log(grid);
 playBtn.addEventListener('click', function() {
     // Svuoto la griglia prima del ciclo così da non avere una nuova griglia ogni volta che premo play
     grid.innerHTML = '';
+    // Mi creo variabile per il punteggio con valore iniziale a 0
+    let score = 0;
     // Per 100 volte dovrò generare un elemento della griglia
     for (let i = 1; i <= 100; i++) {
         // Mi richiamo la funzione per generare un elemento della griglia
